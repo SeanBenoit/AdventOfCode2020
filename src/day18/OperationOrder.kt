addbbbbb7ecea7407030df269d78d3a58208c770
Sean Benoit <sean.joseph.benoit@gmail.com>
2020-12-19 15:51:55 -0500

    Hacky solution to day 18 puzzle 2

--- src/day18/OperationOrder.kt
+++ src/day18/OperationOrder.kt
@@ -17,7 +17,6 @@ class Expression(var parent: Expression? = null) {
     }
 
     fun evaluate(): Long {
-        println("$this $type $value $leftChild $rightChild")
         return when (type) {
             NONE -> {
                 if (leftChild != null) leftChild!!.evaluate()
@@ -39,8 +38,6 @@ fun findEndOfNumber(expressionString: String, startIndex: Int): Int {
 }
 
 fun tokenize(expressionString: String): List<String> {
-    // Surround the whole expression in parens to make building the parse tree easier
-//    val tokens = mutableListOf("(")
     val tokens = mutableListOf<String>()
 
     var i = 0
@@ -58,9 +55,22 @@ fun tokenize(expressionString: String): List<String> {
         i++
     }
 
-//    tokens.add(")")
-
     return tokens
+}
+
+fun findLeftParen(tokens: List<String>, rightParenIndex: Int): Int {
+    var openParens = 1
+    var index = rightParenIndex
+    while (openParens > 0) {
+        index--
+        if (index < 0) break
+        when (tokens[index]) {
+            "(" -> openParens--
+            ")" -> openParens++
+        }
+    }
+
+    return index
 }
 
 fun findRightParen(tokens: List<String>, leftParenIndex: Int): Int {
@@ -78,6 +88,55 @@ fun findRightParen(tokens: List<String>, leftParenIndex: Int): Int {
     return index
 }
 
+fun forceOperatorPrecedence(
+        tokens: MutableList<String>,
+        startIndex: Int,
+        endIndex: Int
+): Pair<Int, Int> {
+    var parensAdded = 0
+    var i = startIndex
+    var newEndIndex = endIndex
+    var startOfLeftOperand = startIndex
+    while (i < newEndIndex) {
+        when (tokens[i]) {
+            "*" -> Unit
+            "+" -> {
+                tokens.add(startOfLeftOperand, "(")
+                parensAdded++
+                i++
+                newEndIndex++
+                val endOfRightOperand = if (tokens[i + 1] == "(") {
+                    findRightParen(tokens, i + 1)
+                } else {
+                    i + 1
+                }
+                tokens.add(endOfRightOperand + 1, ")")
+                parensAdded++
+                newEndIndex++
+            }
+            "(" -> {
+                startOfLeftOperand = i
+                val resultsPair = forceOperatorPrecedence(
+                        tokens,
+                        startOfLeftOperand + 1,
+                        findRightParen(tokens, i)
+                )
+                i = resultsPair.first
+                newEndIndex += resultsPair.second
+                parensAdded += resultsPair.second
+            }
+            ")" -> {
+                startOfLeftOperand = findLeftParen(tokens, i)
+            }
+            else -> startOfLeftOperand = i
+        }
+
+        i++
+    }
+
+    return Pair(newEndIndex, parensAdded)
+}
+
 fun parseExpression(tokens: List<String>): Expression {
     var i = 0
     var currentExpression = Expression()
@@ -85,7 +144,6 @@ fun parseExpression(tokens: List<String>): Expression {
         when (tokens[i]) {
             "(" -> {
                 val matchingParenIndex = findRightParen(tokens, i)
-                println("$i $matchingParenIndex")
                 val subExpression = parseExpression(tokens.subList(i + 1, matchingParenIndex))
                 currentExpression.value = subExpression.value
                 currentExpression.leftChild = subExpression.leftChild
@@ -151,5 +209,13 @@ fun solvePuzzle1(input: List<String>) {
 }
 
 fun solvePuzzle2(input: List<String>) {
+    val tokenLists = input.map { tokenize(it).toMutableList() }
+    tokenLists.forEach { forceOperatorPrecedence(it, 0, it.size) }
 
+    val expressions = tokenLists.map { parseExpression(it) }
+
+    val sum = expressions.map { it.evaluate() }
+            .sum()
+
+    println(sum)
 }
